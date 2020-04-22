da74254e5af04e07f3030ac7d2081995dce87678
Loïc Lopez <loic.lopez@epitech.eu>
2020-04-22 17:50:21 +0200

    Adding better project architecture

---
+++ UMVC.Core/Templating/Generator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UMVC.Templating.Templates;
+
+
+namespace UMVC.Templating
+{
+    public static class Generator
+    {
+        public static void GenerateModel(string modelName, string namespaceName, string outputDir)
+        {
+            ModelTemplate model = new ModelTemplate
+            {
+                //Create our session.
+                Session = new Dictionary<string, object>()
+            };
+
+            model.Session["_ClassNameField"] = modelName;
+            model.Session["_NamespaceField"] = namespaceName;
+
+            model.Initialize();
+
+            string classDef = model.TransformText();
+
+            File.WriteAllText($"{outputDir}/{modelName}.cs", classDef);
+        }
+    }
+}
---
+++ UMVC.Core/Tests/GeneratorTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using NUnit.Framework;
+using UMVC.Templating;
+
+namespace Tests
+{
+    [TestFixture]
+    public class GeneratorTests
+    {
+        [Test]
+        public void TestIfModelAsBeenGenerated()
+        {
+            var currentDir = Directory.GetCurrentDirectory();
+            var namespaceName = "TestNamespace";
+            var modelName = "TestModel";
+            
+            Generator.GenerateModel(modelName, namespaceName, currentDir);
+            var generatedFile = currentDir + "/TestModel.cs";
+            Assert.IsTrue(File.Exists(generatedFile));
+            
+            //SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(File.ReadAllText(generatedFile));
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(File.ReadAllText(generatedFile));
+            
+            // define other necessary objects for compilation
+            string assemblyName = Path.GetRandomFileName();
+            MetadataReference[] references = {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
+            };
+
+            // analyse and generate IL code from syntax tree
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                new[] { syntaxTree },
+                references,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var ms = new MemoryStream())
+            {
+                // write IL code into memory
+                EmitResult result = compilation.Emit(ms);
+                
+                if (!result.Success)
+                {
+                    // handle exceptions
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic => 
+                        diagnostic.IsWarningAsError || 
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                    Assert.Fail("See compiler errors!");
+                }
+                else
+                {
+                    // load this 'virtual' DLL so that we can use
+                    ms.Seek(0, SeekOrigin.Begin);
+                    Assembly assembly = Assembly.Load(ms.ToArray());
+
+                    var desiredClass = $"{namespaceName}.{modelName}";
+                    // create instance of the desired class and call the desired function
+                    Type type = assembly.GetType(desiredClass);
+                    object obj = Activator.CreateInstance(type);
+                    Console.WriteLine(obj);
+                }
+            }
+
+        }
+    }
+}
