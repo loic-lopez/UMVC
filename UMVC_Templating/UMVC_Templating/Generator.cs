23b29127c075495d3ef158e5530ce87de423f776
Loïc Lopez <loic.lopez@epitech.eu>
2020-04-22 15:51:47 +0200

    templatings

---
+++ UMVC_Templating/UMVC_Templating/Generator.cs
@@ -0,0 +1,10 @@
+﻿namespace UMVC.Templating
+{
+    public static class Generator
+    {
+        public static void GenerateModel(string componentName, string outputDir)
+        {
+            
+        }
+    }
+}
