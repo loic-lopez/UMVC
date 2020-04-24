267f4f92a0aefbbb1fded6d30a9985b0fe8926e4
Loïc Lopez <loic.lopez@epitech.eu>
2020-04-24 16:39:14 +0200

    Add working generation of ControllerTemplate

--- UMVC.Core/Tests/GeneratorTests.cs
+++ UMVC.Core/Tests/GeneratorTests.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Emit;
+﻿using System.IO;
 using NUnit.Framework;
-using UMVC.Core.Generation;
 
 namespace Tests
 {
@@ -15,65 +7,29 @@ namespace Tests
     public class GeneratorTests
     {
         [Test]
-        public void TestIfModelAsBeenGenerated()
+        public void TestIfControllerHasBeenGenerated()
         {
+            const string namespaceName = "TestNamespace";
+            const string modelName = "TestModel";
+            const string controllerName = "TestController";
             var currentDir = Directory.GetCurrentDirectory();
-            var namespaceName = "TestNamespace";
-            var modelName = "TestModel";
-            
-            Generator.GenerateModel(modelName, namespaceName, currentDir);
-            var generatedFile = currentDir + "/TestModel.cs";
-            Assert.IsTrue(File.Exists(generatedFile));
-            
-            //SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(File.ReadAllText(generatedFile));
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(File.ReadAllText(generatedFile));
-            
-            // define other necessary objects for compilation
-            string assemblyName = Path.GetRandomFileName();
-            MetadataReference[] references = {
-                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
-            };
 
-            // analyse and generate IL code from syntax tree
-            CSharpCompilation compilation = CSharpCompilation.Create(
-                assemblyName,
-                new[] { syntaxTree },
-                references,
-                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+            var generatedObj = TestsUtils.GenerateController(controllerName, modelName, namespaceName, currentDir);
 
-            using (var ms = new MemoryStream())
-            {
-                // write IL code into memory
-                EmitResult result = compilation.Emit(ms);
-                
-                if (!result.Success)
-                {
-                    // handle exceptions
-                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic => 
-                        diagnostic.IsWarningAsError || 
-                        diagnostic.Severity == DiagnosticSeverity.Error);
+            var desiredClass = $"{namespaceName}.{controllerName}";
+            Assert.IsTrue(generatedObj.GetType().ToString() == desiredClass);
+        }
 
-                    foreach (Diagnostic diagnostic in failures)
-                    {
-                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
-                    }
-                    Assert.Fail("See compiler errors!");
-                }
-                else
-                {
-                    // load this 'virtual' DLL so that we can use
-                    ms.Seek(0, SeekOrigin.Begin);
-                    Assembly assembly = Assembly.Load(ms.ToArray());
+        [Test]
+        public void TestIfModelHasBeenGenerated()
+        {
+            const string namespaceName = "TestNamespace";
+            const string modelName = "TestModel";
+            var currentDir = Directory.GetCurrentDirectory();
 
-                    var desiredClass = $"{namespaceName}.{modelName}";
-                    // create instance of the desired class and call the desired function
-                    Type type = assembly.GetType(desiredClass);
-                    object obj = Activator.CreateInstance(type);
-                    Assert.IsTrue(obj.GetType() == type);
-                }
-            }
-
+            var generatedObj = TestsUtils.GenerateModel(modelName, namespaceName, currentDir);
+            var desiredClass = $"{namespaceName}.{modelName}";
+            Assert.IsTrue(generatedObj.GetType().ToString() == desiredClass);
         }
     }
 }
