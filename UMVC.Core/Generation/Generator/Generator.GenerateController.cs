d57d6d973df72924074d64b0343196c4c709570f
Loïc Lopez <loic.lopez@epitech.eu>
2020-04-30 12:10:26 +0200

    Update template in order to make parameters more flexible

---
+++ UMVC.Core/Generation/Generator/Generator.GenerateController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+// ReSharper disable once RedundantUsingDirective
+using UMVC.Core.Templates;
+
+
+namespace UMVC.Core.Generation.Generator
+{
+    public static partial class Generator
+    {
+        public static void GenerateController(GeneratorParameters.GeneratorParameters generatorParameters)
+        {
+            ControllerTemplate template = new ControllerTemplate
+            {
+                //Create our session.
+                Session = new Dictionary<string, object>()
+            };
+
+            template.Session["ClassName"] = generatorParameters.Controller.Name;
+            template.Session["Namespace"] = generatorParameters.NamespaceName;
+            template.Session["Model"] = generatorParameters.Model.Name;
+            template.Session["Extends"] = generatorParameters.Controller.Extends;
+            template.Session["BaseNamespace"] = generatorParameters.Controller.BaseNamespace;
+
+            template.Initialize();
+
+            string classDef = template.TransformText();
+
+            File.WriteAllText($"{generatorParameters.OutputDir}/{generatorParameters.Controller.Name}.cs", classDef);
+        }
+    }
+}
