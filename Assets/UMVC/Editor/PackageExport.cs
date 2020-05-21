4a2f8b6918c976ec11e5db262df35c720b81d4f4
Loïc Lopez <loic.lopez@epitech.eu>
2020-05-21 12:48:26 +0200

    try to activate unity

---
+++ Assets/UMVC/Editor/PackageExport.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public static class PackageExport
+{
+    // method must be static
+    [MenuItem("UMVC/Export to UnityPackage")]
+    public static void Export()
+    {
+        // configure
+        const string root = "UMVC";
+        const string exportPath = "./UMVC.Editor.unitypackage";
+
+        var path = Path.Combine(Application.dataPath, root);
+        var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
+            .Where(x => Path.GetExtension(x) == ".cs")
+            .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
+            .ToArray();
+
+        Debug.Log("Export below files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
+
+        AssetDatabase.ExportPackage(
+            assets,
+            exportPath,
+            ExportPackageOptions.Default
+        );
+
+        Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
+    }
+}
