e74bb8659b86ef96c7de03537dc6464b00b1056b
Loïc Lopez <loic.lopez@epitech.eu>
2020-05-21 14:24:28 +0200

    try to build a .unitypackage

--- Assets/UMVC/Editor/UMVCPackageExport.cs
+++ Assets/UMVC/Editor/UMVCPackageExport.cs
@@ -7,28 +7,82 @@ using UnityEngine;
 // ReSharper disable once CheckNamespace
 public static class UMVCPackageExport
 {
-    // method must be static
-    [MenuItem("UMVC/Export to UnityPackage")]
-    public static void Export()
+    private const string Root = "UMVC/";
+    private const string ExportWithDllsPath = "./UMVC.Editor.PreBuiltDlls.unitypackage";
+    private const string ExportMsBuildForUnityPath = "./UMVC.Editor.MsBuildForUnity.unitypackage";
+    
+    [MenuItem("UMVC Tools/ExportWithPreBuiltDlls")]
+    public static void ExportWithPreBuiltDlls()
     {
-        // configure
-        const string root = "UMVC";
-        const string exportPath = "./UMVC.Editor.unitypackage";
+        File.Delete(
+            Path.Combine(Application.dataPath, $"{Root}Editor/EditorDependencies/UMVC.Dependencies.msb4u.csproj")
+        );
+        File.Delete(
+            Path.Combine(Application.dataPath, $"{Root}Editor/EditorDependencies/UMVC.Dependencies.msb4u.csproj.meta")
+        );
+        File.Delete(Path.Combine(Application.dataPath, $"{Root}PlayerDependencies/UMVC.Dependencies.msb4u.csproj"));
+        File.Delete(Path.Combine(Application.dataPath, $"{Root}PlayerDependencies/UMVC.Dependencies.msb4u.csproj.meta"));
+        AssetDatabase.Refresh();
+        
+        Export(ExportWithDllsPath);
+    }
 
-        var path = Path.Combine(Application.dataPath, root);
+    [MenuItem("UMVC Tools/ExportWithMsBuildForUnity")]
+    public static void ExportWithMsBuildForUnity()
+    {
+        Directory.Delete(Path.Combine(Application.dataPath, $"{Root}Editor/EditorDependencies"), true);
+        Directory.Delete(Path.Combine(Application.dataPath, $"{Root}PlayerDependencies/"), true);
+        Copy($"{Application.dataPath}/../MSBuildForUnity/PlayerDependencies", $"{Application.dataPath}/UMVC/PlayerDependencies");
+        Copy($"{Application.dataPath}/../MSBuildForUnity/EditorDependencies", $"{Application.dataPath}/UMVC/Editor/EditorDependencies");
+        AssetDatabase.Refresh();
+
+        Export(ExportMsBuildForUnityPath);
+    }
+
+    private static void Export(string destFile)
+    {
+        var path = Path.Combine(Application.dataPath, Root);
         var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
             .Where(x => Path.GetExtension(x) == ".cs")
             .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
             .ToArray();
 
-        Debug.Log("Export below files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
+        Debug.Log("Export bellow files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
 
         AssetDatabase.ExportPackage(
             assets,
-            exportPath,
+            destFile,
             ExportPackageOptions.Default
         );
 
-        Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
+        Debug.Log("Export complete: " + Path.GetFullPath(destFile));
+    }
+    
+    private static void Copy(string sourceDirectory, string targetDirectory)
+    {
+        var diSource = new DirectoryInfo(sourceDirectory);
+        var diTarget = new DirectoryInfo(targetDirectory);
+ 
+        CopyAll(diSource, diTarget);
+    }
+ 
+    private static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+    {
+        Directory.CreateDirectory(target.FullName);
+ 
+        // Copy each file into the new directory.
+        foreach (FileInfo fi in source.GetFiles())
+        {
+            Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+            fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+        }
+ 
+        // Copy each subdirectory using recursion.
+        foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+        {
+            DirectoryInfo nextTargetSubDir =
+                target.CreateSubdirectory(diSourceSubDir.Name);
+            CopyAll(diSourceSubDir, nextTargetSubDir);
+        }
     }
 }
