891dcfda55388d93184842109cbeab54b186340b
Loïc Lopez <loic.lopez@epitech.eu>
2020-05-21 15:31:23 +0200

    Try to pack the two .unitypackage

--- Assets/UMVC/Editor/UMVCPackageExport.cs
+++ Assets/UMVC/Editor/UMVCPackageExport.cs
@@ -10,18 +10,14 @@ public static class UMVCPackageExport
     private const string Root = "UMVC/";
     private const string ExportWithDllsPath = "./UMVC.Editor.PreBuiltDlls.unitypackage";
     private const string ExportMsBuildForUnityPath = "./UMVC.Editor.MsBuildForUnity.unitypackage";
-    
+
     [MenuItem("UMVC Tools/ExportWithPreBuiltDlls")]
     public static void ExportWithPreBuiltDlls()
     {
         File.Delete(
             Path.Combine(Application.dataPath, $"{Root}Editor/EditorDependencies/UMVC.Dependencies.msb4u.csproj")
         );
-        File.Delete(
-            Path.Combine(Application.dataPath, $"{Root}Editor/EditorDependencies/UMVC.Dependencies.msb4u.csproj.meta")
-        );
         File.Delete(Path.Combine(Application.dataPath, $"{Root}PlayerDependencies/UMVC.Dependencies.msb4u.csproj"));
-        File.Delete(Path.Combine(Application.dataPath, $"{Root}PlayerDependencies/UMVC.Dependencies.msb4u.csproj.meta"));
         AssetDatabase.Refresh();
         
         Export(ExportWithDllsPath);
@@ -30,58 +26,48 @@ public static class UMVCPackageExport
     [MenuItem("UMVC Tools/ExportWithMsBuildForUnity")]
     public static void ExportWithMsBuildForUnity()
     {
-        Directory.Delete(Path.Combine(Application.dataPath, $"{Root}Editor/EditorDependencies"), true);
-        Directory.Delete(Path.Combine(Application.dataPath, $"{Root}PlayerDependencies/"), true);
-        Copy($"{Application.dataPath}/../MSBuildForUnity/PlayerDependencies", $"{Application.dataPath}/UMVC/PlayerDependencies");
-        Copy($"{Application.dataPath}/../MSBuildForUnity/EditorDependencies", $"{Application.dataPath}/UMVC/Editor/EditorDependencies");
+        File.Copy(
+            $"{Application.dataPath}/../MSBuildForUnity/EditorDependencies/UMVC.Dependencies.msb4u.csproj", 
+            $"{Application.dataPath}/UMVC/Editor/EditorDependencies/UMVC.Dependencies.msb4u.csproj"
+            ,true
+        );
+        
+        File.Copy(
+            $"{Application.dataPath}/../MSBuildForUnity/PlayerDependencies/UMVC.Dependencies.msb4u.csproj", 
+            $"{Application.dataPath}/UMVC/PlayerDependencies/UMVC.Dependencies.msb4u.csproj"
+            ,true
+        );
         AssetDatabase.Refresh();
-
-        Export(ExportMsBuildForUnityPath);
+        
+        Export(ExportMsBuildForUnityPath, true);
     }
 
-    private static void Export(string destFile)
+    private static void Export(string destFile, bool disableDlls = false)
     {
         var path = Path.Combine(Application.dataPath, Root);
-        var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
-            .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
-            .ToArray();
+        var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories);
 
-        Debug.Log("Export bellow files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
+        if (disableDlls)
+        {
+            assets = assets.Where(x =>
+            {
+                if (Path.GetFileName(x).Equals("UMVC.Dependencies.msb4u.csproj")) return true;
+                
+                return Path.GetExtension(x) != ".dll" && !x.Contains("Dependencies");
+            });
+        }
+
+        assets = assets.Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"));
+
+        var assetList = assets.ToList();
+        Debug.Log("Export bellow files" + Environment.NewLine + string.Join(Environment.NewLine, assetList));
 
         AssetDatabase.ExportPackage(
-            assets,
+            assetList.ToArray(),
             destFile,
             ExportPackageOptions.Default
         );
 
         Debug.Log("Export complete: " + Path.GetFullPath(destFile));
-    }
-    
-    private static void Copy(string sourceDirectory, string targetDirectory)
-    {
-        var diSource = new DirectoryInfo(sourceDirectory);
-        var diTarget = new DirectoryInfo(targetDirectory);
- 
-        CopyAll(diSource, diTarget);
-    }
- 
-    private static void CopyAll(DirectoryInfo source, DirectoryInfo target)
-    {
-        Directory.CreateDirectory(target.FullName);
- 
-        // Copy each file into the new directory.
-        foreach (FileInfo fi in source.GetFiles())
-        {
-            Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
-            fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
-        }
- 
-        // Copy each subdirectory using recursion.
-        foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
-        {
-            DirectoryInfo nextTargetSubDir =
-                target.CreateSubdirectory(diSourceSubDir.Name);
-            CopyAll(diSourceSubDir, nextTargetSubDir);
-        }
     }
 }
