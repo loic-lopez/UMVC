6b90838cd3878a884fca7d891e70c52635f8a0d7
Loïc Lopez <loic.lopez@epitech.eu>
2020-05-21 16:24:31 +0200

    Create release workflow (#30)
    Create the release workflow on master branch

---
+++ .github/workflows/scripts/install-umvc.editor-deps-same-workflow.sh
@@ -0,0 +1,16 @@
+ls
+
+moveDllToDir() {
+  mv "$1" "$2"
+  echo "Moving dependency $1 to $2"
+}
+
+
+# player deps
+moveDllToDir "UMVC.Core.Exceptions.dll" "Assets/UMVC/PlayerDependencies"
+moveDllToDir "UMVC.Core.MVC.dll" "Assets/UMVC/PlayerDependencies"
+
+# Editor deps
+moveDllToDir "UMVC.Core.Components.dll" "Assets/UMVC/Editor/EditorDependencies"
+moveDllToDir "UMVC.Core.Generation.dll" "Assets/UMVC/Editor/EditorDependencies"
+moveDllToDir "UMVC.Core.Templates.dll" "Assets/UMVC/Editor/EditorDependencies"
