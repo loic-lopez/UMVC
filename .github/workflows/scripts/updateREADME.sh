49b6a945f5814fc2ec5e3df6e9a0a6811056664d
Loïc Lopez <loic.lopez@epitech.eu>
2020-06-03 16:41:09 +0200

    [skip ci] fix updateREADME.sh: Bad substitution

--- .github/workflows/scripts/updateREADME.sh
+++ .github/workflows/scripts/updateREADME.sh
@@ -2,8 +2,10 @@ TAG=$1
 
 echo "Release version: $TAG"
 
-RELEASE_VERSION="${TAG:1}" # escape the "v" of "v1.0.0"
+RELEASE_VERSION="$(echo ${TAG} | cut -c 2-)" # escape the "v" of "v1.0.0"
 SEARCH='\$VERSION\$'
 
+echo "Updating README.md to version: $TAG..."
 sed "s/${SEARCH}/${RELEASE_VERSION}/g" README.template.md > README.md
+echo "Done updating README.md to version: $TAG!"
 
